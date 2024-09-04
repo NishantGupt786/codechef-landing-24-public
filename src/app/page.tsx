021b697bf6173f1a02684756682514cee6f8cd08
Harshit Kashyap Sarma <88274018+kashyap-harshit@users.noreply.github.com>
2024-09-04 22:17:16 +0530

    boardPages (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Events from '@/components/sections/Events';
+import BoardPage from  '@/components/boardPage/BoardPage';
+import BlogCard from "@/components/BlogCard";
+
 export default function Home() {
   return (
-      <main>
-        <Events/>
-      </main>
+    <main>
+      {/* <Events/> */}
+      <BoardPage />
+      
+    </main>
   );
 }

f2ed972b3e1006b105092d7ad709a321e8e0051e
Samya Mehta <142434600+samyamehta16@users.noreply.github.com>
2024-09-04 21:28:56 +0530

    Smooth Scrolling (#7)
    * blogs added
    
    * structural changes made
    
    * moved assets
    
    * smooth scrolling added
    
    * changes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Component1 from "@/components/sections/Component1";
-import Component2 from "@/components/sections/Component2";
 import Events from '@/components/sections/Events';
-
 export default function Home() {
   return (
-    <main>
-      <Events/>
-    </main>
+      <main>
+        <Events/>
+      </main>
   );
 }

3cc11aa5245d2b31d6fd4b95eef45dbf23f59903
yashxita <gouriyashita@gmail.com>
2024-09-07 23:56:03 +0530

    line paths added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 import { Inter } from "next/font/google";
 import React from "react";
 import "../styles/globals.css";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +23,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${inter.className} overflow-x-hidden w-screen`}>
           {children}
-        <Footer />
+        
         </body>
       </html>
     </ReactLenis>

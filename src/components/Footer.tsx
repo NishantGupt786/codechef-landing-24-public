f5ca9ec972d9247f31f5c4f15682ecb1f7deb809
Harshit Kashyap Sarma <88274018+kashyap-harshit@users.noreply.github.com>
2024-09-11 00:08:59 +0530

    responsive footer (#17)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,34 +35,33 @@ const Footer: React.FC = () => {
     <footer className="bg-black text-white pt-10">
       <div className="text-center mb-4 mt-44 font-enigma">
         <span
-          className={`text-6xl sm:text-9xl ${
-            isAtBottom ? "text-[#FF3B00]" : "text-white"
-          }`}
+          className={`text-5xl sm:text-7xl md:text-8xl ${isAtBottom ? "text-[#FF3B00]" : "text-white"
+            }`}
         >
           CODECHEF
         </span>
       </div>
-      <div className="grid grid-cols-2 border-t-2 border-b-2 border-white font-enigma">
-        <p className="text-xl mx-auto p-4">
+      <div className="sm:grid grid-cols-2 flex flex-col justify-center items-center border-t-2 border-b-2 border-white font-enigma">
+        <p className="sm:text-left text-center sm:text-xl text-base mx-auto p-4">
           CODECHEF-VIT IS A NON-COMMERCIAL ORGANISATION WITH A GOAL TO PROVIDE A
           PLATFORM FOR PROGRAMMERS AND DEVELOPERS EVERYWHERE TO MEET, COMPETE &
           HAVE FUN. AT CODECHEF-VIT, WE BELIEVE IN THE WORDS OF MATT MULLENWEG –
           “TECHNOLOGY IS THE BEST WHEN IT BRINGS PEOPLE TOGETHER”.
         </p>
-        <div className="grid grid-cols-2 border-l-2 border-white grid-container ">
-          <div className="grid-item grid grid-cols-2 w-[700px] h-[50px]">
-            <FooterButton label={"HOME"} redirect={"#"}/>
-            <FooterButton label={"PROJECTS"} redirect={"#"}/>
+        <div className="sm:w-auto w-full grid item-center grid-row-3 sm:border-l-2 border-white grid-container sm:h-full">
+          <div className="grid-item grid grid-cols-2 w-full h-[50px]">
+            <FooterButton label={"HOME"} redirect={"#"} />
+            <FooterButton label={"PROJECTS"} redirect={"#"} />
           </div>
-          <br></br>
-          <div className="grid-item grid grid-cols-2  w-[700px]  h-[50px]">
-          <FooterButton label={"BOARD"} redirect={"#"}/>
-            <FooterButton label={"DEVSOC"} redirect={"#"}/>
+
+          <div className="grid-item grid grid-cols-2  w-full  h-[50px]">
+            <FooterButton label={"BOARD"} redirect={"#"} />
+            <FooterButton label={"DEVSOC"} redirect={"#"} />
           </div>
-          <br></br>
-          <div className="grid-item grid grid-cols-2   w-[700px]  h-[50px]">
-          <FooterButton label={"EVENTS"} redirect={"#"}/>
-            <FooterButton label={"BLOG"} redirect={"#"}/>
+
+          <div className="grid-item grid grid-cols-2   w-full  h-[50px]">
+            <FooterButton label={"EVENTS"} redirect={"#"} />
+            <FooterButton label={"BLOG"} redirect={"#"} />
           </div>
         </div>
       </div>
@@ -154,9 +153,15 @@ const FooterButton = ({
 }) => {
   return (
     <Link href={redirect}>
-      <div className="bg-gray-800 text-white p-2 sm:p-3 text-center rounded hover:bg-[#FF3B00] transition hex4 border-b-4 sm:border-b-8 border-r-2 sm:border-r-4 border-white">
-        {label}
+      <div className="h-full relative">
+        <div className="absolute w-full bg-white  rounded transition hex4 p-2 sm:p-4 ">
+          p
+        </div>
+        <div className="absolute w-full right-[5px] top-[-5px] bg-gray-800 text-white p-2 py-[11px] sm:p-4 text-center rounded hover:bg-[#FF3B00] transition hex4 text-xs sm:text-base">
+          {label}
+        </div>
       </div>
     </Link>
   );
 };
+

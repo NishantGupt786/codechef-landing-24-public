60714f78bf2233b601832da1f5676cda8259bbd8
Aq <64064721+AqViolet@users.noreply.github.com>
2024-10-21 22:29:37 +0530

    Navbar compo (#29)
    * backup commit
    
    * navbar add
    
    * Delete pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Abhinav Ganeshan <56132559+Abh1noob@users.noreply.github.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,8 @@
+
+import React from "react";
+import Image from "next/image";
+import { nav } from "@/lib/navicons";
+
 const Navbar = () => {
   return (
     <div className="container">
@@ -7,31 +12,31 @@ const Navbar = () => {
         </button>
         <div className="cn-wrapper" id="cn-wrapper">
           <ul>
-            <li>
-              <a href="#">
-                <span className="icon-picture"></span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon-headphones"></span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon-home"></span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon-facetime-video"></span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <span className="icon-envelope-alt"></span>
-              </a>
-            </li>
+          <li>
+  <a href="#">
+    <span className="fa fa-gear"></span>
+  </a>
+</li>
+<li>
+  <a href="#">
+    <span className="fa fa-headphones"></span>
+  </a>
+</li>
+<li>
+  <a href="#">
+    <span className="fa fa-home"></span>
+  </a>
+</li>
+<li>
+  <a href="#">
+    <span className="fa fa-video"></span>
+  </a>
+</li>
+<li>
+  <a href="#">
+    <span className="fa fa-envelope"></span>
+  </a>
+</li>
           </ul>
         </div>
         <div id="cn-overlay" className="cn-overlay"></div>
@@ -39,5 +44,52 @@ const Navbar = () => {
     </div>
   );
 };
+
+
+// import React from "react";
+// import { nav } from "@/lib/navicons";
+
+// const Navbar = () => {
+//   return (
+//     <div className="container">
+//       <div className="component">
+//         <button className="cn-button" id="cn-button">
+//           +
+//         </button>
+//         <div className="cn-wrapper" id="cn-wrapper">
+//           <ul>
+//             <li>
+//               <a href="#">
+//                 <img src={nav.grid} alt="grid icon" style={{ width: "30px", height: "30px", backgroundColor: "black" }} />
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#">
+//                 <img src={nav.home} alt="home icon" style={{ width: "30px", height: "30px", backgroundColor: "black" }} />
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#">
+//                 <img src={nav.list} alt="list icon" style={{ width: "30px", height: "30px", backgroundColor: "black" }} />
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#">
+//                 <img src={nav.message} alt="message icon" style={{ width: "30px", height: "30px", backgroundColor: "black" }} />
+//               </a>
+//             </li>
+//             <li>
+//               <a href="#">
+//                 <img src={nav.user} alt="user icon" style={{ width: "30px", height: "30px", backgroundColor: "black" }} />
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//         <div id="cn-overlay" className="cn-overlay"></div>
+//       </div>
+//     </div>
+//   );
+// };
+
 
 export default Navbar;

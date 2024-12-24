e263357abd6d2fa7c332e303cb25fc360c05c98c
DhruvsChopra <dhruvsinghchopra70@gmail.com>
2024-12-24 13:59:43 +0530

    Adding loader for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,25 @@
+
 "use client";
-import ClientComponent from "@/components/ClientComponent";
+
+import ClientComponent from "@/components/ClientComponent"; 
 import CodeChefHeader from "@/components/HomePage";
 import Ripple from "@/components/Ripple";
+
 import { useEffect, useState } from "react";
+
+
 const HomeMain: React.FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean | null>(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const hasLoadedBefore = sessionStorage.getItem("hasLoadedBefore");
-
-      if (hasLoadedBefore) {
-        setIsLoading(false);
-      } else {
-        setIsLoading(true);
-        const timer = setTimeout(() => {
-          setIsLoading(false);
-          sessionStorage.setItem("hasLoadedBefore", "true");
-        }, 15000);
-
-        return () => clearTimeout(timer);
-      }
-    }
-  }, []);
   return (
     <main>
-<ClientComponent isLoading={isLoading}>
-  <div className="relative bg-white">
-    <div>
-      <CodeChefHeader />
-    </div>
-    <div className="absolute top-0 left-0 w-full h-full -z-10">
-      <Ripple />
-    </div>
-  </div>
-</ClientComponent>
-
+      <div className="relative bg-white">
+        <div>
+          <CodeChefHeader />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full -z-10">
+          <Ripple />
+        </div>
+      </div>
     </main>
   );
 };

617f94dbc72447884fc2413548daae3dea51c07c
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-12-24 20:20:55 +0530

    someone on stackoverflow said u shouldn't put max-scale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({
             name="viewport"
             content="height=device-height, 
                       width=device-width, initial-scale=1.0, 
-                      minimum-scale=1.0, maximum-scale=1.0, 
+                      minimum-scale=1.0, 
                       user-scalable=no, target-densitydpi=device-dpi"
           ></meta>
         </head>
